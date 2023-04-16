d09fd9e67faf76702947159346ac3ea883fbc177
GuiiFernandes <guifjj92@gmail.com>
2023-04-16 16:43:19 -0300

    projeto playground functions 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,22 +1,99 @@
 // Desafio 1 - Crie a função compareTrue
+const compareTrue = (value1, value2) => value1 && value2;
 
 // Desafio 2 - Crie a função splitSentence
+const splitSentence = (phrase) => phrase.split(' ');
 
 // Desafio 3 - Crie a função concatName
+const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 
 // Desafio 4 - Crie a função footballPoints
+const footballPoints = (wins, ties) => wins * 3 + ties * 1;
 
 // Desafio 5 - Crie a função highestCount
+const highestCount = (array) => {
+  const bigger = Math.max(...array);
+  return array.reduce((counter, value) => {
+    if (value === bigger) counter += 1;
+    return counter;
+  }, 0);
+};
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+const calcTriangleArea = (base, height) => (base * height) / 2;
+const calcRectangleArea = (base, height) => (base * height);
+const calcAllAreas = (base, height, form) => {
+  if (form === 'triângulo') {
+    return `O valor da área do triângulo é de: ${calcTriangleArea(base, height)}`;
+  }
+  if (form === 'retângulo') {
+    return `O valor da área do retângulo é de: ${calcRectangleArea(base, height)}`;
+  }
+  return 'Não foi possível fazer o cálculo, insira uma forma geométrica válida';
+};
 
 // Desafio 7 - Crie a função catAndMouse
+const catAndMouse = (mouse, cat1, cat2) => {
+  const distanceCat1 = Math.abs(cat1 - mouse);
+  const distanceCat2 = Math.abs(cat2 - mouse);
+  if (distanceCat1 === distanceCat2) return 'os gatos trombam e o rato foge';
+  return distanceCat1 > distanceCat2 ? 'cat2' : 'cat1';
+};
 
 // Desafio 8 - Crie a função fizzBuzz
+const fizzBuzz = (array) => array.map((value) => {
+  if (value % 3 === 0 && value % 5 === 0) return 'fizzBuzz';
+  if (value % 5 === 0) return 'buzz';
+  if (value % 3 === 0) return 'fizz';
+  return 'bug!';
+});
 
 // Desafio 9 - Crie a função encode e a função decode
+const code = (phrase, codes) => {
+  let newPhrase = '';
+  for (let character of phrase) {
+    if (codes[character] !== undefined) {
+      newPhrase += codes[character];
+    } else {
+      newPhrase += character;
+    }
+  }
+  return newPhrase;
+};
+
+function encode(phrase) {
+  const codes = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return code(phrase, codes);
+}
+
+function decode(phrase) {
+  const codes = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return code(phrase, codes);
+}
 
 // Desafio 10 - Crie a função techList
+const techList = (array, name) => {
+  const newArray = [];
+  for (let item of array.sort()) {
+    newArray.push({
+      tech: item,
+      name,
+    });
+  }
+  return newArray;
+};
 
 // Não modifique essas linhas
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,46 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+const sizeArray = (array) => {
+  if (array.length !== 11) return 'Array com tamanho incorreto.';
+};
+
+const countRepeat = (num, array) => array.reduce((counter, value) => {
+  if (value === num) return counter + 1;
+  return counter;
+}, 0);
+
+const analyzeNumbers = (array) => {
+  for (let num of array) {
+    if (num < 0 || num > 9 || countRepeat(num, array) >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+};
+
+const generatePhoneNumber = (array) => {
+  let result;
+  result = sizeArray(array);
+  if (!result) result = analyzeNumbers(array);
+  if (!result) {
+    result = `(${array.slice(0, 2).join('')})`;
+    result += ` ${array.slice(2, 7).join('')}-${array.slice(7).join('')}`;
+  }
+  return result;
+};
 
 // Desafio 12 -  Crie a função triangleCheck
+const triangleCheck = (lA, lB, lC) => lA < lB + lC && lB < lA + lC && lC < lA + lB;
 
 // Desafio 13 - Crie a função hydrate
+const hydrate = (phrase) => {
+  const arrayNumbers = Array.from(phrase.matchAll(/[1-9]/g));
+  let sum = 0;
+  for (let num of arrayNumbers) {
+    sum += Number(num[0]);
+  }
+  let plural = '';
+  if (sum > 1) plural = 's';
+  return `${sum} copo${plural} de água`;
+};
 
 /* eslint no-undef: 0 */
 
